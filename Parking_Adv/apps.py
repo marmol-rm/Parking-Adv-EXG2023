6b949762dee467031a69aac308c63ba5d723e6cc
Marcelo Molina <34018240+marmol-rm@users.noreply.github.com>
2023-09-07 23:30:15 -0600

    Instalando componentes

---
+++ Parking_Adv/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ParkingAdvConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Parking_Adv'
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Parking-Adv-EXG2023
 Proyecto de desarrollo para la fase 2 del EXG2023
+
+Creado por: Marcelo Alberto Molina Romero
