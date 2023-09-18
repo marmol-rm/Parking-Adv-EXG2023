27e7f74a9b5e98b308fd6b78558a89846e76adf9
Marcelo Molina <117409407+marmol-echo@users.noreply.github.com>
2023-09-17 19:50:09 -0600

    Database config

--- Parking_Adv_EXG2023/settings.py
+++ Parking_Adv_EXG2023/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Parking_Adv_EXG2023.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_parking_adv',
+        'USER': 'marmol',
+        'PASSWORD': 'mr14015',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
